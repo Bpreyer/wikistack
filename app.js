c827ea0db23f120bbf0fa8cc4e43bf04caaba576
Britt Preyer <B.A.PreyerJr@gmail.com>
2020-06-25 12:29:57 -0400

    first push

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const morgan = require('morgan');
 const express = require('express');
+// const client = require("./db");
 const app = express()
-const static = express.static();
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+const { db } = require('./models');
+
+
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.send('.views/layout')
 })
+
+db.authenticate().
+then(() => {
+  console.log('You are connected to the Database');
+})
+
+
+
+
+
+
+
+
+
+
+
+const PORT = 1337;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
+
+
