1a7b25c48ad63966d5752e7f8cef91e61d76fa7d
Britt Preyer <B.A.PreyerJr@gmail.com>
2020-06-25 16:47:28 -0400

    workign on user

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const { userList } = require("../views");
+const { userPages } = require("../views");
+
+
+router.get("/", (req, res) => {
+  res.send(userPages());
+});
+
+router.post("/", (req, res) => {
+  res.send("got to POST /wiki");
+});
+
+router.get("/add", (req, res) => {
+  res.send(userList());
+});
 
 
 module.exports = router;
